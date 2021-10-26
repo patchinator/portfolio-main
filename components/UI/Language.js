b2edd162f504cfb2b6c0346c70510013312f0ec5
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-10-26 22:33:12 +0100

    added remaining cards. added images for remaining Card components

--- components/UI/Language.js
+++ components/UI/Language.js
@@ -8,18 +8,39 @@ const Language = (props) => {
     if (color === "Ruby on Rails") return "rgb(198,0,0)";
     if (color === "Cloudinary") return "rgb(52,72,197)";
     if (color === "PostreSQL") return "rgb(51,103,145)";
-    if (color === "JSX") return "rgb(63,204,251)";
+    if (color === "Javascript") return "rgb(239,216,29)";
     if (color === "Next.js") return "white";
     if (color === "Chakra-UI") return "rgb(58,199,189)";
     if (color === "Firebase") return "rgb(255,203,45)";
+    if (color === "React") return "rgb(63,204,251)";
     if (color === "Vercel") return "black";
     if (color === "SCSS") return "rgb(199,100,148)";
+    return "rgb(240, 240, 240)";
+  };
+
+  const fontColorHandler = (font) => {
+    if (font === "HTML5") return "white";
+    if (font === "CSS") return "white";
+    if (font === "Ruby") return "white";
+    if (font === "Ruby on Rails") return "white";
+    if (font === "Cloudinary") return "white";
+    if (font === "PostreSQL") return "white";
+    if (font === "Javascript") return "black";
+    if (font === "Next.js") return "black";
+    if (font === "Chakra-UI") return "black";
+    if (font === "Firebase") return "black";
+    if (font === "React") return "black";
+    if (font === "Vercel") return "white";
+    if (font === "SCSS") return "black";
   };
 
   return (
     <div
       className={style.language}
-      style={{ background: colorHandler(props.color), color: props.font }}
+      style={{
+        background: colorHandler(props.color),
+        color: fontColorHandler(props.font),
+      }}
     >
       {props.name}
     </div>
