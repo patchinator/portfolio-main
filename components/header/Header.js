90601aad62a9dd0f5892b0020b78511e1e7f21eb
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-10-27 00:35:45 +0100

    installed framer motion for animation effects. Added animation to Header component

--- components/header/Header.js
+++ components/header/Header.js
@@ -2,14 +2,34 @@ import { Fragment } from "react";
 import style from "./Header.module.scss";
 import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { motion } from "framer-motion";
 
-const Header = () => {
+const Header = (props) => {
+  const titleVariants = {
+    titleHidden: { opacity: 0 },
+    titleVisible: { opacity: 1 },
+  };
+
   return (
     <Fragment>
       <section className={style.container}>
         <div className={style.details}>
-          <h1>Patrick Hoveman</h1>
-          <h2>Junior Web Developer</h2>
+          <motion.h1
+            initial="titleHidden"
+            animate="titleVisible"
+            variants={titleVariants}
+            transition={{ duration: 1.5 }}
+          >
+            {props.name}
+          </motion.h1>
+          <motion.h2
+            initial="titleHidden"
+            animate="titleVisible"
+            variants={titleVariants}
+            transition={{ duration: 2 }}
+          >
+            {props.role}
+          </motion.h2>
           <div>
             <button>
               <FontAwesomeIcon icon={faArrowDown} />
