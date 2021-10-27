2ae567bce182bd782d4a47e4a1ba1d06f28f6ef5
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-10-27 01:05:07 +0100

    added effect hook to display About component on view

--- components/about/About.js
+++ components/about/About.js
@@ -1,22 +1,34 @@
+import { useEffect } from "react";
 import Image from "next/image";
 import style from "./About.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBook } from "@fortawesome/free-solid-svg-icons";
 import { motion } from "framer-motion";
 import { useAnimation } from "framer-motion";
+import { useInView } from "react-hook-inview";
 
 const About = (props) => {
   const controls = useAnimation();
   const [ref, inView] = useInView();
 
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
+
   return (
     <section className={style.container}>
       <motion.div
+        ref={ref}
         className={style.about_wrapper}
         initial="hidden"
-        animate="visible"
-        variants={{ hidden: { opacity: 0 }, visible: { opacity: 1 } }}
-        transition={{ duration: 2 }}
+        animate={controls}
+        variants={{
+          hidden: { opacity: 0 },
+          visible: { opacity: 1 },
+        }}
+        transition={{ duration: 1.2 }}
       >
         <div className={style.title}>
           <h1>About</h1>
