ad358368d73eb60cfa1c2d77c62a1fec71bf3ce5
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-10-28 10:46:54 +0100

    added logos for footer. Created logo SCSS file to import to all components with logo

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,9 +1,13 @@
 import style from "./Footer.module.scss";
 import button from "../../styles/UI/buttons.module.scss";
+import logo from "../../styles/UI/logo.module.scss";
 
+import { Link } from "react-scroll";
+
+// icons
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope, faArrowUp } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-scroll";
+import { faGithubAlt, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
@@ -18,11 +22,39 @@ const Footer = () => {
             />
           </Link>
           <p>Get in contact</p>
-          <button>
-            <a href="">
-              <FontAwesomeIcon width="2rem" height="2rem" icon={faEnvelope} />
-            </a>
-          </button>
+          <div>
+            <button className={button.icon_button}>
+              <a
+                href="mailto:patrickhoveman@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon width="2rem" height="2rem" icon={faEnvelope} />
+              </a>
+            </button>
+            <button className={button.icon_button}>
+              <a
+                href="mailto:patrickhoveman@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon
+                  width="2rem"
+                  height="2rem"
+                  icon={faGithubAlt}
+                />
+              </a>
+            </button>
+            <button className={button.icon_button}>
+              <a
+                href="mailto:patrickhoveman@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon width="2rem" height="2rem" icon={faLinkedin} />
+              </a>
+            </button>
+          </div>
         </div>
       </div>
     </section>
